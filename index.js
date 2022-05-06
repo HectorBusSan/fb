8f88718323c9999cb9d6293d015eaade7ca0e183
Hector <hector141599@gmail.com>
2022-05-05 20:12:48 -0500

    Iniciando Refactoring

---
+++ index.js
@@ -0,0 +1,11 @@
+// const Reader = require('./lib/utils/Reader')
+// const ExplorerServices = require('./lib/services/ExplorerService');
+
+// const explorers= Reader.readJsonFile("explorer.json");
+// // console.log(explorers);
+
+// // aplicación  del Explorer Services sobre la lista del explorer
+// console.log(ExplorerServices.filterByMission(explorers,"node"));
+// ExplorerServices.getAmountOfExplorersByMission(explorers,"node");
+// ExplorerServices.getExplorersUsernamesByMission(explorers,"node");
+
