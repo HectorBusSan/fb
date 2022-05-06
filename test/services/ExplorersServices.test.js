3a3bcaf315820c46c5862ec58ee1fe203fc06041
Hector <hector141599@gmail.com>
2022-05-05 21:53:30 -0500

    Agregando npm install --save-dev jest@26.0.0

--- test/services/ExplorersServices.test.js
+++ test/services/ExplorersServices.test.js
@@ -2,7 +2,7 @@ const ExplorerService= require('./../../lib/services/ExplorerService');
 describe('Test para ExplorerServices',()=>{
     test("Requerimientos 1: Calcular todos los explorers en una misión",()=>{
         const explorers= [{mission:"node"}];
-        const explorerInNode=ExplorerService.filterByMission(explorers,"node");
+        const explorerInNode= ExplorerService.filterByMission(explorers,"node");
         expect(explorerInNode.length).toBe(1);
     })
 });
