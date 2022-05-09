f541e765ac76d94651242f46dd3f470a13532317
Hector <hector141599@gmail.com>
2022-05-09 12:29:53 -0500

    Agregando pruebas en index.js

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const Reader = require("./lib/utils/Reader");
 const ExplorerServices = require("./lib/services/ExplorerService");
+const Fizzbuz= require("./lib/services/FizzbuzzService");
+const ExplorersControllers= require("./lib/controllers/ExplorersControllers");
 
+console.log("----------Reader-------------");
 const explorers= Reader.readJsonFile("explorer.json");
 // console.log(explorers);
 
 // aplicación  del Explorer Services sobre la lista del explorer
+console.log("----------ExplorerServices-------------");
 console.log(ExplorerServices.filterByMission(explorers,"node"));
 // console.log(ExplorerServices.getAmountOfExplorersByMission(explorers,"node"));
 // console.log(ExplorerServices.getExplorersUsernamesByMission(explorers,"node"));
-
+console.log("----------FizzBuzz-------------");
+console.log(Fizzbuz.applyValidationInExplorer({name: "Explorer1",score:1}));
+console.log(Fizzbuz.applyValidationInExplorer({name: "Explorer3",score:3}));
+console.log(Fizzbuz.applyValidationInExplorer({name: "Explorer5",score:5}));
+console.log(Fizzbuz.applyValidationInExplorer({name: "Explorer15",score:15}));
+console.log("----------Controls------------");
+console.log(ExplorersControllers.getExplorersByMission("node"));
+console.log("");
+console.log(ExplorersControllers.getExplorersUsernamesByMission("node"));
+console.log("");
+console.log(ExplorersControllers.getExplorersAmonutByMission("node"));
